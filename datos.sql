3f4d94186030c757006861ccdf82f31ee1a1ca57
StillEffect6 <96755454+StillEffect6@users.noreply.github.com>
2023-04-19 23:16:00 -0400

    Add files via upload

---
+++ datos.sql
@@ -0,0 +1,12 @@
+INSERT INTO Obra (id_obra, nombre_obra, autor, nacionalidad, anio, tipo, alto, ancho, id_museo) 
+VALUES
+(1, 'La Gioconda', 'Leonardo da Vinci', 'Italiano', 1503, 'Renacimiento', 77, 53, 17),
+(2, 'Las Meninas', 'Diego Velazquez', 'Espanola', 1656, 'Barroco', 518, 876, 18),
+(3, 'La Noche Estrellada', 'Vincent van Gogh', 'Neerlandes', 1889, 'Impresionismo', 73, 88, 12),
+(4, 'Guernica', 'Pablo Picasso', 'Espanola', 1937, 'Cubismo', 349, 776, 22),
+(5, 'La ultima cena', 'Leonardo da Vinci', 'Italiano', 1498, 'Barroco', 460, 880, 29),
+(6, 'La Creacion de Adan', 'Miguel Angel', 'Italiano', 1511, 'Renacimiento', 280, 570, 3),
+(7, 'La Persistencia de la Memoria', 'Salvador Dali', 'Espanol', 1931, 'Surrealismo', 24, 33, 13),
+(8, 'El Grito', 'Edvard Munch', 'Noruego', 1893, 'Expresionismo', 91, 74, 7),
+(9, 'El Nacimiento de Venus', 'Sandro Botticelli', 'Italiano', 1486, 'Renacimiento', 172, 278, 8),
+(10, 'El Jardín de las Delicias', 'El Bosco', 'Neerlandes', 1505, 'Primitivos flamencos de la escuela flamenca', 220, 389, 18),
