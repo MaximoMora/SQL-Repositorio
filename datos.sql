f4a5bb21ff39d7698600e68083cb099e512a4be7
StillEffect6 <96755454+StillEffect6@users.noreply.github.com>
2023-04-20 03:23:05 -0400

    Update datos.sql

--- datos.sql
+++ datos.sql
@@ -1,12 +1,12 @@
 INSERT INTO Obra (id_obra, nombre_obra, autor, nacionalidad, anio, tipo, alto, ancho, id_museo) 
 VALUES
-(1, 'La Gioconda', 'Leonardo da Vinci', 'Italiano', 1503, 'Renacimiento', 77, 53, 17),
-(2, 'Las Meninas', 'Diego Velazquez', 'Espanola', 1656, 'Barroco', 518, 876, 18),
-(3, 'La Noche Estrellada', 'Vincent van Gogh', 'Neerlandes', 1889, 'Impresionismo', 73, 88, 12),
-(4, 'Guernica', 'Pablo Picasso', 'Espanola', 1937, 'Cubismo', 349, 776, 22),
-(5, 'La ultima cena', 'Leonardo da Vinci', 'Italiano', 1498, 'Barroco', 460, 880, 29),
-(6, 'La Creacion de Adan', 'Miguel Angel', 'Italiano', 1511, 'Renacimiento', 280, 570, 3),
-(7, 'La Persistencia de la Memoria', 'Salvador Dali', 'Espanol', 1931, 'Surrealismo', 24, 33, 13),
-(8, 'El Grito', 'Edvard Munch', 'Noruego', 1893, 'Expresionismo', 91, 74, 7),
-(9, 'El Nacimiento de Venus', 'Sandro Botticelli', 'Italiano', 1486, 'Renacimiento', 172, 278, 8),
-(10, 'El Jardín de las Delicias', 'El Bosco', 'Neerlandes', 1505, 'Primitivos flamencos de la escuela flamenca', 220, 389, 18),
+(31, 'A La luz de Temuco', 'Leonardo da Vinci', 'Italiano', 1503, 'Renacimiento', 8000, 1562, 17),
+(32, 'Las golondrinas', 'Kevin Mora', 'Espanola', 1656, 'Barroco', 518, 876, 18),
+(33, 'La Noche callada', 'Sebastian Mena', 'Chileno', 1889, 'Impresionismo', 73, 1, 12),
+(34, 'La Luz de la UCT', 'Pablo Picasso', 'Espanola', 1937, 'Cubismo', 4, 10, 22),
+(35, 'La ultima 7b', 'Leonardo da Vinci', 'Italiano', 1498, 'Barroco', 460, 880, 29),
+(36, 'La Creacion de Python', 'Candela Vargas', 'Espanol', 1511, 'Renacimiento', 280, 570, 3),
+(37, 'La Sodexo de la Memoria', 'Lucas Fierro', 'Chileno', 1931, 'Surrealismo', 24, 336, 13),
+(38, 'El Certamen de matematicas', 'Abril Vargas', 'Noruego', 1893, 'Expresionismo', 10, 5, 7),
+(39, 'El Nacimiento la Feria pinto', 'Fernando Alonso', 'Espanol', 1486, 'Renacimiento', 172, 278, 8),
+(40, 'Zetas del Jardin de la UCT', 'Pablo Picasso', 'Espanol', 1505, 'Primitivos flamencos de la escuela flamenca', 600, 389, 18);
